36f6420d48df10713250e1d9b1c08a77d13c548a
CodeMode365 <codemode365@gmail.com>
2022-12-12 15:47:47 +0000

    signup files created

---
+++ Pages/Login/Form/Signup.js
@@ -0,0 +1,37 @@
+const form = document.querySelector(".Signup-form");
+const submitBtn = document.querySelector("#signup-btn");
+// const errorText = form.querySelector(".form form .error-txt");
+// console.log(errorText);
+
+form.onsubmit = (e) => {
+  e.preventDefault(); //preventing form from submitting
+};
+submitBtn.onclick = () => {
+  // ajax here
+  let xhr = new XMLHttpRequest(); //creating xml object
+  xhr.open("POST", "../../../Back/Signup.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let data = xhr.response;
+
+      if (data == "Account created successfully") {
+        // errorText.style.background = "green";
+        // errorText.style.color = "white";
+        // errorText.textContent = data;
+        // errorText.style.display = "block";
+        // window.location = "users.php";
+        alert("Account registerd");
+      } else {
+        // errorText.style.background = "#f8d7da";
+        // errorText.style.color = "#721c24";
+        // errorText.textContent = data;
+        // errorText.style.display = "block";
+        alert("Registration error");
+      }
+    }
+  };
+  //sendind data from form using ajax
+  let formData = new FormData(form); //from object
+  xhr.send(formData);
+  console.log('ok')
+};
